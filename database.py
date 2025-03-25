a32c973fa7400774d3f2cf40d0eee6a576c23e6b
Малий Денис <160033406+MaliiDenis@users.noreply.github.com>
2025-03-25 12:48:02 +0200

    database.py

--- database.py
+++ database.py
@@ -1,13 +1,13 @@
 import sqlite3
 
 def init_db():
-    conn = sqlite3.connect('tasks.db')
-    cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS tasks (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        title TEXT)''')
-    conn.commit()
-    conn.close()
+    conn = sqlite3.connect('tasks.db')
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS tasks (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        text TEXT NOT NULL)''')
+    conn.commit()
+    conn.close()
 
 if __name__ == "__main__":
-    init_db()
+    init_db()
